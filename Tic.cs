540b625a478008f0cb679d6601338b7d06c6245b
Darshit01 <32360312+Darshit01@users.noreply.github.com>
2019-04-12 12:14:03 -0400

    Update Tic.cs

--- Tic.cs
+++ Tic.cs
@@ -1,5 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace Tic-Tac-Toe{
   
