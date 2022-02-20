bafe5a21dde69e9080556e7680adc2770cb4cd50
Darshit01 <32360312+Darshit01@users.noreply.github.com>
2022-02-20 13:20:22 -0500

    Add files via upload

---
+++ tic.js
@@ -0,0 +1,9 @@
+function ask(){
+var ask = prompt("Would you like to play this game??");
+if(ask === "yes" || ask === "Yes"){
+    alert("ok time to go");
+}else{
+    alert("Sorry you may exit by closing this window");
+    
+}
+}
